c21631d70f657ba8a46e75f39c567f44f85d2ffc
deptno <deptno@gmail.com>
2019-10-21 11:29:09 +0900

    feat: UI focus 처리 추가

--- src/components/li/LiHappyBox.tsx
+++ src/components/li/LiHappyBox.tsx
@@ -1,14 +1,33 @@
-import React, {useCallback, useContext} from 'react'
+import React, {useCallback, useContext, useMemo} from 'react'
 import {HappyBox} from '../../builder/model'
 import {MapContext} from '../../context/map'
+import * as R from 'ramda'
 
 export const LiHappyBox: React.FC<Props> = props => {
+  const {data, focus, onClick} = props
+  const {호선, 역명, 위치정보, 계, 열, 제어부, 소형, 중형, 대형, x, y} = data
   const map = useContext(MapContext)
-  const move = useCallback(() => map.setCenter(new kakao.maps.LatLng(y, x)), [map])
-  const {호선, 역명, 위치정보, 계, 열, 제어부, 소형, 중형, 대형, x, y} = props.data
+  const move = useCallback(
+    R.compose(
+      () => map.setCenter(new kakao.maps.LatLng(y, x)),
+      R.tap(
+        R.compose(
+          onClick,
+          R.always(역명),
+        ),
+      ),
+    ),
+    [map],
+  )
+  const css = useMemo(() =>
+      focus
+        ? 'flex lh-copy bg-blue white'
+        : 'flex lh-copy hover-bg-light-blue hover-white'
+    , [focus],
+  )
 
   return (
-    <li className="flex lh-copy hover-bg-blue hover-white" onClick={move}>
+    <li className={css} onClick={move}>
       <span className="w3">{호선}</span>
       <span className="w5">{역명}</span>
       <span className="w-100">{위치정보}</span>
@@ -24,4 +43,6 @@ export const LiHappyBox: React.FC<Props> = props => {
 
 type Props = {
   data: HappyBox
+  onClick(args): void
+  focus: boolean
 }
--- src/components/ul/UlHappyBoxes.tsx
+++ src/components/ul/UlHappyBoxes.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {HappyBox} from '../../builder/model'
 import {LiHappyBox} from '../li/LiHappyBox'
 import {LiHappyBoxHeader} from '../li/LiHappyBoxHeader'
+import * as R from 'ramda'
 
 export const UlHappyBoxes: React.FC<Props> = props => {
+  const [station, select] = useState<string>()
+
   return (
     <ul className="list ph2 f6 w-100">
+      {station}
       <LiHappyBoxHeader/>
-      {props.data.map(box => <LiHappyBox key={box.역명} data={box}/>)}
+      {props.data.map(box => <LiHappyBox key={box.역명} data={box} onClick={select} focus={R.equals(station, box.역명)}/>)}
     </ul>
   )
 }
