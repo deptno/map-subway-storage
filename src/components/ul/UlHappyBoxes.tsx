049a6e993aa13159b350e47e86ab65903e4311c9
bglee <deptno@gmail.com>
2020-02-08 18:37:28 +0900

    feat: 선택된 역에 대해 컬러 표시

--- src/components/ul/UlHappyBoxes.tsx
+++ src/components/ul/UlHappyBoxes.tsx
@@ -5,11 +5,11 @@ import {LiHappyBoxHeader} from '../li/LiHappyBoxHeader'
 import * as R from 'ramda'
 
 export const UlHappyBoxes: React.FC<Props> = props => {
-  const [station, select] = useState<string>()
+  const [station, select] = useState<string>('역을 선택하세요.')
 
   return (
     <ul className="list ph2 f6 w-100">
-      {station}
+      <span className="ph2 pv1 br2 bg-black white">{station}</span>
       <LiHappyBoxHeader/>
       {props.data.map(box => <LiHappyBox key={box.역명} data={box} onClick={select} focus={R.equals(station, box.역명)}/>)}
     </ul>
