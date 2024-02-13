61b9e0a63a0729a395f2677670b045a8a005483b
deptno <deptno@users.noreply.github.com>
2024-02-13 18:26:27 +0000

    deploy: 34ca5a59b40009e317565cd26a8c73d7f6bb4598

---
+++ _next/static/hkM5PDwX4Xy2GVj1d1XYy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/195-4335a3b800cc45f5.js","static/chunks/pages/index-ee797a6cd1270d2b.js"],"/_error":["static/chunks/pages/_error-11ea4afd2d1ea625.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
